6ab45ccadf032ba36ef3660ec9b8d39fe04b7548
Nick Porillo <nsporillo@gmail.com>
2016-03-01 15:25:04 -0800

    Fix exception with head setting

--- src/main/java/net/porillo/skullwalls/commands/SetHeadCommand.java
+++ src/main/java/net/porillo/skullwalls/commands/SetHeadCommand.java
@@ -1,14 +1,15 @@
 package net.porillo.skullwalls.commands;
 
-import com.google.common.collect.ImmutableSet;
 import net.porillo.skullwalls.SkullWalls;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.block.Block;
 import org.bukkit.block.Skull;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class SetHeadCommand extends BaseCommand {
@@ -23,9 +24,10 @@ public class SetHeadCommand extends BaseCommand {
 
     public void runCommand(CommandSender sender, List<String> args) {
         if (args.size() == 1) {
-            Block b = ((Player) sender).getTargetBlock(ImmutableSet.of(), 6);
+            Block b = ((Player) sender).getTargetBlock((HashSet<Byte>)null, 6);
             if (b.getType().equals(Material.SKULL)) {
                 Skull skull = (Skull) b.getState();
+                skull.setSkullType(SkullType.PLAYER);
                 skull.setOwner(args.get(0));
                 skull.update();
                 sender.sendMessage(ChatColor.BLUE + "Set SkullOwner to \"" + ChatColor.GOLD + args.get(0) + ChatColor.BLUE + "\"");
