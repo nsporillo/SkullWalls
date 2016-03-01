aef5580d2fde0b6ac370b7f2ac32bc08c64d9e71
Nick Porillo <nsporillo@gmail.com>
2016-03-01 01:19:42 -0800

    Only display available wall types to user

--- src/main/java/net/porillo/skullwalls/walls/WallType.java
+++ src/main/java/net/porillo/skullwalls/walls/WallType.java
@@ -1,5 +1,11 @@
 package net.porillo.skullwalls.walls;
 
+import com.google.common.base.Joiner;
+
 public enum WallType {
-    GLOBAL, CUSTOM, BANNED
+    GLOBAL, CUSTOM, BANNED;
+
+    public static String getTypes() {
+        return Joiner.on(",").join(values());
+    }
 }
